6ca0059eb336be644582e65e03bbf6459a425614
mckaymatt3 <mckaymatt@outlook.com>
2022-02-06 14:05:14 -0500

    commit tree 575beda11a8a6d0596acd77ee64bd86ff1a49c0f

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code goes here
+// Your code goes 
+document.addEventListener("DOMContentLoaded", function (){
+    // select the paragraph by the ID using querySelector
+    const selectParagraph = document.querySelector('#text')
+    console.log(selectParagraph)
+    // change the text of the paragraph
+    const changeText = selectParagraph.innerHTML = "This is really cool!"
+    console.log(changeText)
+    // change the color of the text
+    const changeColor = selectParagraph.style.color = "blue"
+    console.log(changeColor)
+    // change the size of the text
+    selectParagraph.style.fontSize = '300px'
+})
